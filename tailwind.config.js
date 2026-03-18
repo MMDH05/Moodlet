fbc00b362979d5faad1d4639e472f510392f25ea
MMDH05 <mikaela.herrera@seven-gen.com>
2026-03-19 01:36:19 +0800

    update base styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Nunito', 'system-ui', 'sans-serif'],
       },
     },
   },
